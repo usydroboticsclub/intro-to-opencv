bf1e9fd8b02a38bb52d1720da6d37c411017ea1f
acenturyandabit <steeven.liu2@gmail.com>
2021-01-13 13:24:18 +1100

    fix numbering in challenges

--- 2_1_drawStuff.py
+++ 2_1_drawStuff.py
@@ -22,7 +22,7 @@ cv2.waitKey(-1)
 # 1. Try to draw a disney Mickey Mouse logo (a few circles) with openCV. Make it white on black.
 # Think about how you can use the cv2.circle command to help ease the process. Have your window be a 600x600 size.
 # 2. Now try to draw the logo black on white instead of white on black.
-# 2. Draw an even more complex image using polygons and lines, and send us the image!
-# 3. [CHALLENGE] Use the polylines function to draw a sine wave on a canvas.
-# 4. [CHALLENGE] Make a little bouncing ball animation and save it to an mp4 file. This stackoverflow post might help:
+# 3. Draw an even more complex image using polygons and lines, and send us the image!
+# 4. [CHALLENGE] Use the polylines function to draw a sine wave on a canvas.
+# 5. [CHALLENGE] Make a little bouncing ball animation and save it to an mp4 file. This stackoverflow post might help:
 # https://stackoverflow.com/questions/30509573/writing-an-mp4-video-using-python-opencv
