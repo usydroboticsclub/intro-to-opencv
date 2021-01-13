5b8eaa66456ef65ab7dbe685107fa5acd6c7f4d8
Marco T <marcoalec@gmail.com>
2021-01-13 12:51:16 +1100

    challenge 1
    disney draw challenge

---
+++ CH_1_drawDisney.py
@@ -0,0 +1,7 @@
+'''In this challenge, you'll be drawing Disney logo using the vector commands you learned earlier.
+Think about how you can use the cv2.circle command to help ease the process. Have your window be a 600x600 size.
+If you're feeling up to it, draw an even more complex image using polygons and lines, and send us the image!'''
+
+
+
+#Write code below!
